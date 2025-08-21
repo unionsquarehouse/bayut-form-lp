329d4762fdc0ee6845d7294fa5f4ca199c152138
unionsquarehouse <support@ushre.com>
2025-08-21 12:41:53 +0400

    no field mandatory

--- src/app/api/leads/route.js
+++ src/app/api/leads/route.js
@@ -4,33 +4,24 @@ export async function POST(request) {
   try {
     const data = await request.json();
     
-    // Validate required fields
-    if (!data.name || !data.email || !data.phone || !data.source) {
-      return Response.json(
-        { error: "Missing required fields" },
-        { status: 400 }
-      );
-    }
-
     // Prepare the data for Bitrix24 API
     const bitrixData = {
       fields: {
-        UF_CRM_DEAL_1652196828525: "Bayut Leads",
-        UF_CRM_1749797976461: data.title || "Generic",
-        SOURCE_ID: data.source ,
+        UF_CRM_1749797976461: data?.title || "Generic",
+        SOURCE_ID: data?.source ,
         PHONE: [
           {
-            VALUE: data.phone,
+            VALUE: data?.phone,
             VALUE_TYPE: "WORK"
           }
         ],
         EMAIL: [
           {
-            VALUE: data.email,
+            VALUE: data?.email,
             VALUE_TYPE: "WORK"
           }
         ],
-        NAME: data.name
+        NAME: data?.name
       }
     };
 
@@ -48,7 +39,7 @@ export async function POST(request) {
     }
 
     console.log(bitrixData, "bitrixData");
-    
+
     // Axios POST request to Bitrix24
     const axiosResponse = await axios.post(
       process.env.BITRIX_ADD_LEADS_URL,
